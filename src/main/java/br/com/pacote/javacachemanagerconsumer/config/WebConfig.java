d492a83e84e7c7b64b891254ed19a4c9e9c80638
ronaldomatias <ronaldomts476@gmail.com>
2024-11-01 19:31:53 -0300

    feat: RedisConnectionFactory

--- src/main/java/br/com/pacote/javacachemanagerconsumer/config/WebConfig.java
+++ src/main/java/br/com/pacote/javacachemanagerconsumer/config/WebConfig.java
@@ -1,6 +1,7 @@
 package br.com.pacote.javacachemanagerconsumer.config;
 
 import br.com.ronaldomatias.cachemanager.aspect.CacheAspect;
+import br.com.ronaldomatias.cachemanager.redis.client.RedisConnectionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +11,12 @@ public class WebConfig {
 	@Bean
 	public CacheAspect getCacheAspect() {
 		return new CacheAspect();
+	}
+
+	@Bean
+	public RedisConnectionFactory redisConnectionFactory() {
+		RedisConnectionFactory.configure("localhost", 6379);
+		return RedisConnectionFactory.getInstance();
 	}
 
 }
